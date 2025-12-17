9e1d9f394bb4a70233ba74abcec5151d6a51bf74
hilmar008 <hilmar008@gmail.com>
2025-12-17 17:42:42 +0100

    HausAufgabenStellung für 18.12. gepullt !

--- ErsterProjekt/BedingungUebungUndSchleifen.cs
+++ ErsterProjekt/BedingungUebungUndSchleifen.cs
@@ -56,6 +56,68 @@
 
 
 
+            //Von Nico gepullt:
+
+            //string benutzername = "Bob123";
+
+            //Console.WriteLine("Geben Sie bitte Ihr neues Passwort ein:");
+            //string? passwort = Console.ReadLine();
+
+            //while (passwort.Length < 10)
+            //{
+            //    Console.WriteLine("Versuchen Sie es bitte nochmals. Das Passwort muss mind. 10 Zeichen enthalten.");
+            //    passwort = Console.ReadLine();
+            //}
+
+            //Console.WriteLine($"Dein Konto wurde erstellt. " +
+            //    $"Sie haben folgende Zugangsdaten definiert:" +
+            //    $"\nBenutzername: {benutzername}\n" +
+            //    $"Passwort: {passwort}");
+
+
+            //int zahl = 1;
+
+            //while (zahl < 100)
+            //{
+            //    Console.WriteLine("Die Aktuelle Zahl ist: " + zahl);
+
+            //    if (Console.ReadLine() == "quit")
+            //    {
+            //        Console.WriteLine("Die Schleife wird terminiert!");
+            //        break;
+            //    }
+            //}
+
+            //int zahl = 1;
+
+            //while (zahl < 100)
+            //{
+            //    zahl += 10;
+
+            //    if (zahl > 30 && zahl < 50)
+            //    {
+            //        Console.WriteLine("Diese Zahlen moegen wir nicht!");
+            //        continue;
+            //    }
+
+            //    Console.WriteLine("Die Aktuelle Zahl ist: " + zahl);
+            //}
+            //Console.WriteLine("Schleife Vorbei!");
+
+            //HA:
+            //Es soll einen PAP erstellt werden und dannach ein programm geschrieben.
+            //Ein Benutzer soll einen Konto erstellen.
+            //Es gelten folgende regeln:
+            //1. Das Benutzername muss mind. 5 Zeichen lang sein.
+            //2. Das Passwort muss mind. 10 Zeichen lang sein und mit das buchstabe 'A' beginnen.
+            //Sobald eine von diese Faelle nicht stimment, dann soll der Benutzer erneut die Daten eingeben.
+            //Hinweis 1 schleife fuer Benutzername 1 schleife fuer passwort
+
+
+
+
+
+
         }
     }
 }
